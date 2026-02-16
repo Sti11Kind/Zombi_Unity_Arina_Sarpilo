1f39061fec274b6bbc4ea56467a90d76bce5fb64
Arina <ari4games0@gmail.com>
2026-02-16 14:50:30 +0200

    new system,jump,rotation,selection

--- Zombi_Uni/Assets/Zombies/Assets/Script/GameManager.cs
+++ Zombi_Uni/Assets/Zombies/Assets/Script/GameManager.cs
@@ -1,18 +1,61 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject[] zombies;
     public GameObject selectedZombie;
+    public Vector3 selectedSize;
+    public Vector3 pushForce;
+    private InputAction next, prev;
+    private InputAction jump;
+    private int selectedIndex = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        selectedZombie = zombies[2];
+        next = InputSystem.actions.FindAction("NextZombie");
+        prev = InputSystem.actions.FindAction("PrevZombie");
+        jump = InputSystem.actions.FindAction("Jump");
+        selectZombie(selectedIndex);
+
+    }
+
+    void selectZombie(int index)
+    {
+       if (selectedZombie != null)
+        {
+            selectedZombie.transform.localScale = Vector3.one;
+        }
+        selectedZombie = zombies[index];
+        selectedZombie.transform.localScale = selectedSize;
+        Debug.Log("selected: " + selectedZombie);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (next.WasPressedThisFrame())
+        {
+            Debug.Log("next");
+            selectedIndex++;
+            if (selectedIndex >= zombies.Length)
+                selectedIndex = 0;
+            selectZombie(selectedIndex); 
+        }
+        if (prev.WasPressedThisFrame())
+        {
+            Debug.Log("prev");
+            selectedIndex--;
+            if (selectedIndex < 0)
+                selectedIndex = 3;
+            selectZombie(selectedIndex);
+        }
+        if (jump.WasPressedThisFrame()) 
+        {
+            Debug.Log("JUMP");
+            Rigidbody rb = selectedZombie.GetComponent<Rigidbody>();
+            if (rb != null)
+                rb.AddForce(pushForce);
+        }
     }
 }
